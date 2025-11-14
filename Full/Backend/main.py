7af20e1109ec760639e45a8d5092c4b17f5648ff
ScatterRaven <kht081725@gmail.com>
2025-11-14 19:00:18 +0900

    BACK : 로그 분석 & 리포트 자동화 구현 <add> prompt.py : 프롬프트 분리 <add> enhanced_report.py : 로그 분석 및 리포트 스케쥴러 포함 <add> doc_converter/ : 프로토타입 pdf 변환 툴 <edit> .gitignore : 위치 변경 및 예외 대상 추가 <edit> query.py : enhanced_report에서 요구되는 쿼리 추가 <edit> config.py: 사용자 디렉토리 관련 코드 추가 <edit> api/__init__.py : 선행코드 추가 <edit> module/__init__.py : main <-> enhanced_report 연동 관련 코드 추가 <edit> chat_agent.py : 불용 코드 일부 제거, prompt 연동 <edit> main.py : enhanced_report 연동

--- Full/Backend/main.py
+++ Full/Backend/main.py
@@ -1,10 +1,11 @@
+import asyncio
 from fastapi import FastAPI,Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from api import chat,login,admin,superadmin, faq
-
+from module import Scheduler_ARP
 
 # CORS 설정
 origins = [
@@ -26,24 +27,29 @@ app.add_middleware(
 # app.mount("/static/images", StaticFiles(directory="page_images"), name="static_images")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/",response_class=HTMLResponse) # 리액트 연결 후 수정 예정 현재는 MVP를 위해서 임시로 작성 이후 router-> api로 풀더 이름 변경
-async def main_page(request:Request):
-    return templates.TemplateResponse("main.html",{"request":request})
+# @app.get("/",response_class=HTMLResponse) # 리액트 연결 후 수정 예정 현재는 MVP를 위해서 임시로 작성 이후 router-> api로 풀더 이름 변경
+# async def main_page(request:Request):
+#     return templates.TemplateResponse("main.html",{"request":request})
 
-@app.get("/login",response_class=HTMLResponse) # 리액트 연결 후 수정 예정 현재는 MVP를 위해서 임시로 작성 이후 router-> api로 풀더 이름 변경
-async def main_page(request:Request):
-    return templates.TemplateResponse("login.html",{"request":request})
+# @app.get("/login",response_class=HTMLResponse) # 리액트 연결 후 수정 예정 현재는 MVP를 위해서 임시로 작성 이후 router-> api로 풀더 이름 변경
+# async def main_page(request:Request):
+#     return templates.TemplateResponse("login.html",{"request":request})
 
-@app.get("/register",response_class=HTMLResponse) # 리액트 연결 후 수정 예정 현재는 MVP를 위해서 임시로 작성 이후 router-> api로 풀더 이름 변경
-async def main_page(request:Request):
-    return templates.TemplateResponse("register.html",{"request":request})
-@app.get("/chat/{pid}",response_class=HTMLResponse) # 리액트 연결 후 수정 예정 현재는 MVP를 위해서 임시로 작성 이후 router-> api로 풀더 이름 변경
-async def main_page(request:Request,pid:str):
-    return templates.TemplateResponse("chat.html",{"request":request,"pid":pid})
+# @app.get("/register",response_class=HTMLResponse) # 리액트 연결 후 수정 예정 현재는 MVP를 위해서 임시로 작성 이후 router-> api로 풀더 이름 변경
+# async def main_page(request:Request):
+#     return templates.TemplateResponse("register.html",{"request":request})
+# @app.get("/chat/{pid}",response_class=HTMLResponse) # 리액트 연결 후 수정 예정 현재는 MVP를 위해서 임시로 작성 이후 router-> api로 풀더 이름 변경
+# async def main_page(request:Request,pid:str):
+#     return templates.TemplateResponse("chat.html",{"request":request,"pid":pid})
+
 
 app.include_router(chat.router, tags=["chat"])
 app.include_router(login.router, tags=["login"],prefix="/api")
 app.include_router(faq.router, tags=["faq"])
             
+
+@app.on_event("startup")
+async def set_scheduler():
+    asyncio.create_task(Scheduler_ARP())
 
 
