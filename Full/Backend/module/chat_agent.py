0af4b88a5aad30a5dd8f8c364946384baf249be2
GUKHOJeong <rnrgh135@naver.com>
2025-11-26 17:19:38 +0900

    tool 추가 및 css 수정

--- Full/Backend/module/chat_agent.py
+++ Full/Backend/module/chat_agent.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal,List
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import ToolNode
@@ -10,6 +10,8 @@ from module.qa_service import HybridRAGChain
 from core.prompt import agent_prompt
 from typing import List, Dict, Any, Optional
 from langchain_core.runnables import RunnableConfig
+from module.qa_recommend import RecommendRAGChain
+from sqlalchemy import text 
 Tool_name={
     "product_qa_tool":"질문",
     "recommend_tool":"추천"
@@ -39,14 +41,34 @@ async def product_qa_tool(query: str, product_id:str,session_id:str) -> str:
     return answer["answer"]
 
 @tool
-async def recommend_tool(product_id:str,count:int=3, *, config: RunnableConfig) -> str:
+async def recommend_tool(product_id:str, config: RunnableConfig = None,count:int=3) -> str:
     """
-    상푼 추천을 해줍니다. 만약 유저가 'count'개 만큼 추천해달라고 하면 count 수만큼 추천을 해주고 작성을 하지않으면 기본값을 사용합니다.
+    상품을 추천을 해줍니다. 만약 유저가 'count'개 만큼 추천해달라고 하면 count 수만큼 추천을 해주고 작성을 하지않으면 기본값을 사용합니다.
     """
-    db_session = config.get("configurable", {}).get("db_session")
-    print("연결됨")
-    data = [{"id":"abc","name":"거대한풍선"},{"id":"cde","name":"거대한선풍기"},{"id":"efg","name":"작은 선풍기"}]
-    return data[:count]
+    db_session = config.get("configurable", {}).get("db") if config else None
+    origin_query = """
+    SELECT product_id,product_name FROM tb_product WHERE category = (SELECT category FROM tb_product WhERE product_id = :product_id) and product_id != :product_id """
+    results = await db_session.execute(text(origin_query),
+    params={
+        "product_id":product_id
+    })
+    code_row = results.mappings().all()
+    return code_row[:count]
+
+@tool
+async def compare_tool(query: str, product_id:str,rec_product_id:List[str],session_id:str):
+    """
+    기존 상품과 비교 상품을 찾아서 사용자가 원하는 키워드를 비교해줍니다.
+
+    query: 비교할 기준 (예: "기능 비교해줘", "가격 차이 알려줘")
+    product_id: 기존 상품 ID
+    rec_product_id: 비교할 대상 상품 ID들의 리스트 (문자열 리스트)
+    session_id: 세션 ID
+
+    """
+    rag = RecommendRAGChain(product_id)
+    answer = await rag.compare_invoke(query,rec_product_id,session_id)
+    return answer["answer"]
 
 
 
@@ -58,7 +80,7 @@ class  ChatBotAgent:
         temperature=0,
         streaming=True,
     )
-        self.tools = [product_qa_tool,recommend_tool]
+        self.tools = [product_qa_tool,recommend_tool,compare_tool]
         self.checkpoint = MemorySaver()
         self.graph =self._build_graph()
         self.session_id = session_id    
@@ -91,9 +113,8 @@ class  ChatBotAgent:
             response = llm_with_tools.with_config({"run_name":"final_answer"}).invoke([system_msg]+state["messages"])
             return {"messages":[response]}
 
-        async def tool_node(state):
+        async def tool_node(state,config):
             last_msg = state["messages"][-1]
-            
             if hasattr(last_msg,"tool_calls") and last_msg.tool_calls: #마지막 메세지에 too_calls 속성이 있고 값이 있으면
                 print(last_msg.tool_calls[0]["name"])
                 tool_name = last_msg.tool_calls[0]["name"]
